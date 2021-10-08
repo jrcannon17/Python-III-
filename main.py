ce9300b936b23300808d11e48c13309f3d5f5227
J.R <jrcannon@indiana.edu>
2021-10-07 20:52:44 -0400

    adding existing file

--- main.py
+++ main.py
@@ -1,78 +1,100 @@
-#! C:\Users\July\Pictures\2020-08
+# This is a sample Python script.
 
-import time
-import os
-import hashlib
-from hashlib import *
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-
-class Duplython:
-    def __init__(self):
-        self.home_dir = os.getcwd(); self.File_hashes = []
-        self.Cleaned_dirs = []; self.Total_bytes_saved = 0
-        self.block_size = 65536; self.count_cleaned = 0
-
-    def welcome(self)->None:
-        print('******************************************************************')
-        print('****************        DUPLYTHON      ****************************')
-        print('********************************************************************\n\n')
-        print('----------------        WELCOME        ----------------------------')
-        time.sleep(3)
-        print('\nCleaning .................')
-
-    def generate_hash(self, Filename:str)->str:
-        Filehash = sha256()
-        try:
-            with open(Filename, 'rb') as File:
-                fileblock = File.read(self.block_size)
-                while len(fileblock)>0:
-                    Filehash.update(fileblock)
-                    fileblock = File.read(self.block_size)
-                Filehash = Filehash.hexdigest()
-            return Filehash
-        except:
-            return False
-    def clean(self)->None:
-        all_dirs = [path[0] for path in os.walk('.')]
-        for path in all_dirs:
-            os.chdir(path)
-            All_Files =[file for file in os.listdir() if os.path.isfile(file)]
-            for file in All_Files:
-                filehash = self.generate_hash(file)
-                if not filehash in self.File_hashes:
-                    if filehash:
-                        self.File_hashes.append(filehash)
-                        #print(file)
-                else:
-                    byte_saved = os.path.getsize(file); self.count_cleaned+=1
-                    self.Total_bytes_saved+=byte_saved
-                    os.remove(file); filename = file.split('/')[-1]
-                    print(filename, '.. cleaned ')
-            os.chdir(self.home_dir)
-
-    def cleaning_summary(self) -> None:
-        mb_saved = self.Total_bytes_saved / 1048576
-        mb_saved = round(mb_saved, 2)
-        print('\n\n--------------FINISHED CLEANING ------------')
-        print('File cleaned  : ', self.count_cleaned)
-        print('Total Space saved : ', mb_saved, 'MB')
-        print('-----------------------------------------------')
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-    def main(self) -> None:
-        self.welcome();
-        self.clean();
-        self.cleaning_summary()
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    App = Duplython()
-    App.main()
+    print_hi('PyCharm')
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+# Models Years 1999 And Later
+#Prefix
+Years = {"A": 2010 ,"B": 2011, "C": 2012, "D": 2013, "E": 2014, "F": 2015, "G": 2016, "H": 2017,
+         "J": 2018, "K": 2019, "L": 2020, "M": 2021, "N": 2022, "P": 2023, "R": 2024,
+         "S": 1995, "T": 1996, "V": 1997, "W": 1998, "X": 1999, "Y": 2000,
+         1:2001, 2:2002,3:2003,4:2004,5:2005,6:2006,7:2007,8:2008,9:2009}
+#Product Line Codes
+PL = {
 
-'''file = open('some.txt', 'rb').read()
-print('this is the file/',file)
+"S1": "Aspire", "S2": "Contour", "S4": "Escort/Focus", "S5": "Ka", "S6": "Fiesta",
+"S7":"Mondeo", "S8": "Probe", "S9": "Mystique",
+"M1": "Tracer",
+"F1":"Taurus", "F2": "Windstar", "F3":"Continental", "F4":"Sable", "F5": "Villager", "F6":"Thunderbird",
+"F7": "EN158", "F8": "FW178", "L1":"Bronco", "L2":"Explorer", "L3": "F150/F250", "L4": "Maverick 4X4",
+"L5": "Ranger", "L6": "Aerostar",
+"R1": "Aston-Martin", "R2": "Falcon", "R3": "Mustang", "R4": "Scorpio", "R5": "Fairlane", "R6": "LTD",
+"R8": "Jaguar S-Type", "R9": "Mark VIII",
+"W2":	"Cougar",
+"W3": "Grand Marquis",
+"W4":	"DEW98",
+"W5":	"EW171",
+"W6":	"Thunderbird RWD",
+"W7":	"EN114 Crown Vic",
+"W8":	"Jaguar XK8/XKR",
+"W9":	"Jaguar XJ-Series",
+"C1":	"Transit",
+"C2":	"Econoline",
+"C3":	"Medium Truck",
+"C4":	"L-Series/Cargo-CF Series Truck",
+"U1":"Power Products",
+"U2":	"Motorcraft Brand",
+"U3":	"Outside Sales",
+"U4":	"#3"
+}
+# 7L3Z-14D696-A
 
-file = hashlib.md5(file).hexdigest()
-print('this is the file hash',file)
-'''
+# Design Responsibility Code
+
+R_Code = {
+
+"A": "Light Truck Engineering",
+"B": "Body Engineering",
+"C": "Chassis Engineering",
+"D": "Overseas Product Engineering",
+"E": "Engine Engineering, Engine Product and Manufacturing Engineering",
+"F": "Electronics Division, Product Engineering Offc" ,
+"G": "Electronics Division, Arbor Plant" ,
+"H": "Climate Control Division, Product Engineering Offc" ,
+"J": "Ford Customer Svc. Division Parts and Accessories Engineering Offc" ,
+"K": "Import Component Engineering, Body Engineering" ,
+"L": "Ford Customer Svc. Division Power Products/Ford Cust. Svc. Engineering" ,
+"M": "Performance Operations, Special Vehicle Operations" ,
+"P": "Transmission and Axle Product and Manufacturing Engineering (Automatic Transmissions)" ,
+"R": "Transmission and Axle Product and Manufacturing Engineering (Manual Transmissions)",
+"S": "Light/Heavy Truck Engineering, Truck Special Order Department" ,
+"T": "Heavy Truck Engineering" ,
+"U": "Electrical and Fuel Handling Division, Product Energy Office",
+"V": "Domestic Special Order Engineering Section, Car Special Order and Special Vehicle Engineering" ,
+"W": "Transmission and Axle Product and Manufacturing Engineering (Axle and Driveline)" ,
+"X": "Plastic and Trim Products Division" ,
+"Y": "Ford Customer Service Division (Lincoln and Mercury), Product Analysis and Publications" ,
+"Z": "Ford Customer Service Division (Ford), Product Analysis and Publications" ,
+8: "Electric Vehicle Engineering",
+5: "Ford PN96/UN98 Platform Engineering"
+}
+
+#Part Numbers
+
+Part_Number = {
+
+1000: "Wheels", 2000: "Brakes", 3000: "Suspension & Steering",
+4000: "Axle & Driveshaft", 5200: "Exhaust", 5300-5400 : "Front Springs & Stabilizer",
+5500-5900 : "Rear Springs", 6000 : "Engine", 7000 : "Transmission",
+8000 : "Radiator, Grille, Cooling & Belts", 9000 : "Fuel/Oil Gauges",
+10000 : "Charging & Instruments", 11000 : "Starting", 12000 : "Ignition",
+13000 : "Lamps & Horn", 14000 : "Wiring", 15000 : "Accessories",
+16000: "Molding & Ornaments", 17000 : "Safety Equipment (Jack, Speedometer,Washer, Wipers, Mirrors, Bumpers)", 18000-18100 : Shocks,
+18200-18700 : "Comfort-Convenience Equipment (Floor Mats, Heater, etc.)",
+18800 : "Radio & Antenna", 18900 : "Speakers", 19000 : "Gasoline",
+19100 : "Fire Extinguishers", 19500 : "Custom Accessories & Car Care",
+19600-19900 : "Air Conditioning, etc."
+
+}
